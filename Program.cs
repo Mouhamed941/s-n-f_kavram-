81db252227506c1e4e7a4956e569d80b16ab5c5a
Mouhamed <fardosi1994@gmail.com>
2022-06-01 14:06:27 +0300

    kurucu methodleri eklendi

--- Program.cs
+++ Program.cs
@@ -3,12 +3,13 @@
 // * Private
 // * Internal
 // * Protected
-Calışan calışan1 = new Calışan();
-calışan1.Ad = "Mouhamed";
-calışan1.SoyAdı = "FARDOUSI ";
-calışan1.No = 6542;
-calışan1.Departman = "Bilişim";
+Calışan calışan1 = new Calışan{Ad = "Mouhamed", SoyAdı = "FARDOUSI ", No = 6542, Departman = "Bilişim"};
+Calışan calışan2 = new Calışan{Ad = "Ali", SoyAdı = "FARDOUSI ", No = 6541, Departman = "Montaj"};
+Calışan calışan3 = new Calışan{Ad="Mouhamed",SoyAdı="yılmaz"};
+
 calışan1.CalisanBilgileri();
+calışan2.CalisanBilgileri();
+calışan3.CalisanBilgileri();
 class Calışan
 {
     public string Ad ;
@@ -22,5 +23,17 @@ class Calışan
         System.Console.WriteLine($"Çalışan Soyadi : {SoyAdı}");
         System.Console.WriteLine($"Çalışan No :  {No}");
         System.Console.WriteLine($"Çalışan Departmant : {Departman}");
+    }
+    public Calışan(string ad ,string soyad ,int no ,string departman)
+    {
+        this.Ad = ad;       //this => Bu sınıfın elemanı
+        this.SoyAdı = soyad;
+        this.No = no ;
+        this.Departman = departman;
+    }
+    public Calışan(){}
+    public Calışan(string ad,string soyad){
+        this.Ad = ad;
+        this.SoyAdı = soyad;
     }
 }
